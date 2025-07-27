07f5239099ab8572864fbdf9f331a47f73697d55
TangoSplicer <jasonmaoakley@gmail.com>
2025-07-27 22:42:26 +0100

    pybind-dev inclusion 11

--- cmake/gtest.cmake
+++ cmake/gtest.cmake
@@ -2,6 +2,6 @@ enable_testing()
 
 file(GLOB_RECURSE TEST_SOURCES CONFIGURE_DEPENDS tests/unit/*.cpp tests/integration/*.cpp)
 add_executable(synq_tests ${TEST_SOURCES})
-target_link_libraries(synq_tests PRIVATE pybind11::all)
+target_link_libraries(synq_tests PRIVATE Theads::Threads pybind11::embed)
 target_include_directories(synq_tests PRIVATE ${pybind11_INCLUDE_DIRS} ${pybind11_INCLUDE_DIRS}/pybind11 ${CMAKE_SOURCE_DIR}/src)
 add_test(NAME AllSynQTests COMMAND synq_tests)
